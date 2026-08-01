b3efa0906312b7ecc3f0c02c2c765dc9834ff223
ixviPCs <ixvipcs@gmail.com>
2026-07-31 21:59:16 -0700

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -120,16 +120,6 @@ window.SPRITE_TEMPLATES = [
     image: "assets/base/dream.png"
   },
   {
-    officialId: "Zero Point Sprite: ESD_ZeroPointSprite_Variant_A",
-    name: "Zero Point Sprite",
-    type: "Zero Point",
-    rarity: "Mythic",
-    chance: "0.03%",
-    variant: false,
-    variantType: null,
-    image: "assets/base/zeropoint.png"
-  },
-  {
   officialId: "Punk Sprite: ESD_PunkSprite_Variant_A",
   name: "Punk Sprite",
   type: "Punk",
